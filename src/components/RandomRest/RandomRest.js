9c8677901f80410d1b7bff8f5065c5af1b379f1a
Hunter Monroe <huntis@outlook.com>
2022-11-11 18:04:37 -0700

    Styled nav component and add random to button click

--- src/components/RandomRest/RandomRest.js
+++ src/components/RandomRest/RandomRest.js
@@ -5,7 +5,7 @@ import ballon from '../../ballon.png';
 function RandomRest({ data }) {
   return (
     <section className="random-section">
-      {data.length === 0 && <p> No random</p>}
+      {data.length === 0 && <p> No random please push the random button!</p>}
       <div className="random-rest-container">
         <div className="random-img-container">
           <img className="random-img" src={data.image} />
