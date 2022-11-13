dc9c1bc3720538993489666ea9311743253d2d98
Hunter Monroe <huntis@outlook.com>
2022-11-12 23:26:53 -0700

    Fix welcome component

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -5,11 +5,19 @@ import './Welcome.css'
 
 function Welcome() {
   return (
-    <div className='welcome-container'>
-      <NavLink className='welcome-title'to="/home" > Wizzy Eats </NavLink>
-      <img className='welcome-img'src={Bird} alt='Boy riding Bird from up'></img>
+    <div className="welcome-container">
+      <p className="welcome-description">Welcome to </p>
+      <p className='welcome-title'> Wizzy Eats</p>
+      <p className="welcome-description"> "An app for the indecisive "</p>
+      <NavLink to="/home" className="click-me">
+        Click Me to get started{' '}
+      </NavLink>
+      <img
+        className="welcome-img"
+        src={Bird}
+        alt="Boy riding Bird from up"></img>
     </div>
-  )
+  );
 }
 
 export default Welcome
